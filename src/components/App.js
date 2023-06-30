bc63be8d369c3e7a6978d019454a8f720d1b10ea
Silvia Parada <silviaparadag@yahoo.es>
2023-06-30 17:14:20 +0200

    Add handleSearchByName

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ import callToApi from '../services/api';
 
 const App = ()  => {
   const [characterList, setCharacterList] = useState([]);
-
+  const [searchByName, setSearchByName] = useState('');
+ 
 
   useEffect( () => {
     callToApi().then((cleanData) => {
@@ -17,11 +18,34 @@ const App = ()  => {
 
   }, [])
 
+  
+
+  const handleChangeSearchName = (ev) => {
+    setSearchByName(ev.target.value);
+  };
+
+  const filteredCharacters = characterList.filter( (eachCharacter) => 
+  eachCharacter.name.toLowerCase().includes(searchByName.toLowerCase())
+  );
+
   return (
         <div className="App"> 
         <Header/>
         <main className='main'>
-          <CharacterList characterList={characterList}/>
+          <label className="form__label" htmlFor="search_name">
+              Nombre:
+              <input className="form__input-text" type="text" name="search_name" id="search_name" value={searchByName} onChange={handleChangeSearchName} />
+          </label>
+          <label className="form__label display-block" htmlFor="search_origin">
+              Planeta:
+              <select className="form__input-text" name="search_origin" id="search_origin" >
+                <option value="All">Todos</option>
+                <option value="Krootabulon">Krootabulon</option>
+                <option value="Kyle's Teenyverse">Kyle's Teenyverse</option>
+                <option value="Detoxifier">Detoxifier</option>
+              </select>
+            </label>
+          <CharacterList characterList={filteredCharacters}/>
         </main>
         <Footer/>
         </div>
@@ -29,3 +53,4 @@ const App = ()  => {
 }
 
 export default App;
+
