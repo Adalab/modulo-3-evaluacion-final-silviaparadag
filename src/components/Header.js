9e60de6d29e091c0daead20a33706fb821e249a7
Silvia Parada <silviaparadag@yahoo.es>
2023-06-30 16:47:15 +0200

    Add character list dataApi

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <header className="header">
           <h1 className='header__title'>
-            Hola mundo!
+            ¿Hola mundo!
           </h1>
           <h3 className='header__subtitle'>
             Abrir consola
