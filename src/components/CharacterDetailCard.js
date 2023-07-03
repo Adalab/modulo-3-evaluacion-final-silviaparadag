6e729ae5415fb198046e663908c6746ba05a1143
Silvia Parada <silviaparadag@yahoo.es>
2023-07-03 10:23:42 +0200

    Finish issue #18, #9, #7, #20

--- src/components/CharacterDetailCard.js
+++ src/components/CharacterDetailCard.js
@@ -2,19 +2,44 @@ import { Link } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
 
 const CharacterDetailCard = ({characterSelected}) => {
-    if (characterSelected) {
+    if ( characterSelected.status === 'Alive') {
     return (
    <article className='card__item'>
         <img src={characterSelected.image} alt={characterSelected.name} className='card__item--img'/>
         <h2 className='card__item--title'>{characterSelected.name}</h2>
         <p className='card__item--text'>{characterSelected.species}</p>
-        <p className='card__item--text'>{characterSelected.status}</p>
+        <p className='card__item--text'><i class="fa-solid fa-hand-spock fa-shake"></i>Hey, dude! I'm alive!</p>
         <p className='card__item--text'>{characterSelected.origin}</p>
         <p className='card__item--text'>Nº de episodios: {characterSelected.episodes}</p>
         <Link to="/">Volver</Link>
      </article>
      )
-    } else {
+    } if (characterSelected.status === 'Dead') {
+    return (
+   <article className='card__item'>
+        <img src={characterSelected.image} alt={characterSelected.name} className='card__item--img'/>
+        <h2 className='card__item--title'>{characterSelected.name}</h2>
+        <p className='card__item--text'>{characterSelected.species}</p>
+        <p className='card__item--text'><i class="fa-sharp fa-solid fa-skull fa-beat-fade"></i>Ooops, I'm fucking dead!</p>
+        <p className='card__item--text'>{characterSelected.origin}</p>
+        <p className='card__item--text'>Nº de episodios: {characterSelected.episodes}</p>
+        <Link to="/">Volver</Link>
+     </article>
+     )
+    } if (characterSelected.status === 'Unknown') {
+    return (
+   <article className='card__item'>
+        <img src={characterSelected.image} alt={characterSelected.name} className='card__item--img'/>
+        <h2 className='card__item--title'>{characterSelected.name}</h2>
+        <p className='card__item--text'>{characterSelected.species}</p>
+        <p className='card__item--text'>No se sabe</p>
+        <p className='card__item--text'>{characterSelected.origin}</p>
+        <p className='card__item--text'>Nº de episodios: {characterSelected.episodes}</p>
+        <Link to="/">Volver</Link>
+     </article>
+     )
+    } 
+    else {
         return (
              <NotFoundPage/> 
         )
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,22 @@
 import EachCharacterData from './EachCharacterData';
 
-const CharacterList = ({characterList}) => {
-    const dataList = characterList.map( (eachCharacterData) => <li key={eachCharacterData.id} className="card">
+const CharacterList = ({characterList, searchByName}) => {
+    
+    const dataList = () => {
+        if (characterList.length >= 1) {
+            return characterList.map( (eachCharacterData) => <li key={eachCharacterData.id} className="card">
         <EachCharacterData eachCharacterData={eachCharacterData}/>
-    </li> )
+    </li> )} else {
+        return (<p>El personaje {searchByName} no existe</p>);
+    }
+    }
 
-return (
-    <ul className="cards">
-       {dataList}
-    </ul>
+    return (
+        <ul className="cards">
+        {dataList()}
+        </ul>
 
-);
+    );
 }
 
 export default CharacterList;
