b87db8c585c392b48b12ba444a3fee0c190f77f2
Silvia Parada <silviaparadag@yahoo.es>
2023-10-25 12:07:46 +0200

    Update some text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,23 @@
 import '../styles/layout/Footer.scss';
 
 const Footer = () => {
-    
-    return(
-       <footer className='footer'>
-          <span className="footer__text"> ©  <a href="https://adalab.es/bootcamp-programacion/" target="_blank" rel="noreferrer" className="footer__text--link">Adalab 2023</a></span>
-          <span className="footer__text">Trótula promotion</span>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <span className="footer__text">
+        {' '}
+        ©{' '}
+        <a
+          href="https://www.linkedin.com/in/silviaparadag/"
+          target="_blank"
+          rel="noreferrer"
+          className="footer__text--link"
+        >
+          Silvia Parada
+        </a>
+      </span>
+      <span className="footer__text">2023</span>
+    </footer>
+  );
+};
 
 export default Footer;
